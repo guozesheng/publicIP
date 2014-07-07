1957af193a7eca26302fe6ad3561c14f2cad9ed9
XiaoGuo <guozesheng@gmail.com>
2014-07-08 00:16:16 +0800

    测试OK，可正常使用，需要优化

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
 
 namespace publicIP
 {
@@ -14,6 +16,45 @@ namespace publicIP
         public Form1()
         {
             InitializeComponent();
+
+            NetworkChange.NetworkAvailabilityChanged += new 
+                System.Net.NetworkInformation.NetworkAvailabilityChangedEventHandler(NetworkAvailabilityCallback);
+        }
+
+        private void NetworkAvailabilityCallback(object sender, NetworkAvailabilityEventArgs e)
+        {
+
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface n in adapters)
+            {
+                if (n.Name == "无线网络连接" && n.OperationalStatus == OperationalStatus.Up)
+                {
+                    //textBox1.Text += n.Name + " is " + n.OperationalStatus.ToString();
+                    mytest();
+                }
+            }
+        }
+
+        private void mytest()
+        {
+            WebClient client = new WebClient();
+            int times = 5;
+            int timeout = 2000;
+
+            while (times-- > 0)
+            {
+                string str = client.DownloadString("http://1.guozesheng.sinaapp.com/publicIP");
+
+                if (str == "hello, world")
+                {
+                    break;
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(timeout);
+                    timeout *= 2;
+                }
+            }
         }
     }
 }
